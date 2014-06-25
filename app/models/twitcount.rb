bc2990e1edd815af7ceda35a21c6422ebd3c154b
jonBarcus <barcus.j@gmail.com>
2014-06-25 13:20:29 -0400

    create TwitCount Module to query the TwitterCounter API and return the current number of users.  (might change/expand functionality of module)

---
+++ app/models/twitcount.rb
@@ -0,0 +1,21 @@
+# This module will allow querying of TwitterCounter API
+# http://twittercounter.com/pages/api
+
+module TwitCount
+
+  # twitter_id should be an integer that is the Twitter ID (not username!)
+  # of the account that you would like information for
+  def self.query(twitter_id)
+
+    base_uri = 'http://api.twittercounter.com/'
+    api_key = '?apikey=' + ENV['TWITTER_COUNTER_API']
+    id_number = '&twitter_id=' + twitter_id.to_s
+
+    response = HTTParty.get(base_uri + api_key + id_number)
+
+    # this method currently returns just the current number of followers
+    return response['followers_current']
+
+  end
+
+end
