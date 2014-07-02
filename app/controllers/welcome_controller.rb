0eb6d0ab01035650cf5483a767280b9f2b3ce464
jonBarcus <barcus.j@gmail.com>
2014-07-02 19:30:42 -0400

    set welcome_controller to activate module and return json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,17 @@ class WelcomeController < ApplicationController
   def index
 
     # @number = TwitCount.query(608161934)
+    display_array = Query.last(10)
+    last_ten_count = []
+    last_ten_time = []
+
+    display_array.each do |x|
+      last_ten_count << x.followers
+      last_ten_time << x.created_at
+    end
+
+    render :json => { last_ten_count: last_ten_count,
+                      last_ten_time: last_ten_time }
 
   end
 
